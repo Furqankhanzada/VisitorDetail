4ea1e28da0fbe2718295dfb8cc2cf583e6b2b79d
Fahad Saeed <fahadsaeed237@gmail.com>
2014-09-24 20:26:42 +0500

    index page

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html>
 <head>
     <meta charset=utf-8>
-    <title><user/title>
-    <link href="style.css" media="screen" rel="stylesheet">
+    <title><user</title>
     <script type="text/javascript" src="whoer.notpacked.js"></script>
 
 </head>
@@ -11,15 +10,14 @@
 
 <div>
 
-    <h1>home</h1>
+    <h1>user</h1>
 
 
 </div>
 <script type="text/javascript">
-    scripts.screen( 'sscreen' );
+    scripts.screen( 'screen' );
 </script>
 
-<script type="text/javascript" src="../jquery.min.js"></script>
 <script type="text/javascript" src="script.js"></script>
 
 </body>
