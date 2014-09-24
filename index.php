cd77b407cbef9edc0d1873ccaa61ea671a8d8a70
Fahad Saeed <fahadsaeed237@gmail.com>
2014-09-24 19:47:29 +0500

    init

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <title><user/title>
+    <link href="style.css" media="screen" rel="stylesheet">
+    <script type="text/javascript" src="whoer.notpacked.js"></script>
+
+</head>
+<body>
+
+<div>
+
+    <h1>home</h1>
+
+
+</div>
+<script type="text/javascript">
+    scripts.screen( 'sscreen' );
+</script>
+
+<script type="text/javascript" src="../jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Fahad Saeed on 9/24/14.
+ */
